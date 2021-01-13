13ed3bfa73e66fee7dcadbf68d183f096d079909
Jade McDaniels <jademcdaniels81@gmail.com>
2021-01-13 09:34:00 -0500

    commit tree 8f9eb6844371809e99f018447c14487e9521c840

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+
+class School
+    def initialize(roster)
+        @roster = {}
+    end
+
+    def roster 
+        @roster
+    end 
+
+    def add_student(name, grade)
+        @roster[grade] 
+           if @roster[grade]
+                @roster[grade] << name
+                else
+                @roster[grade] = [name] 
+            end  
+    end     
+
+    def grade(grade)
+        @roster[grade] 
+    end 
+
+    def sort
+        new_hash= {}
+        @roster.each do |grade,student_names|
+            new_hash[grade] = student_names.sort
+        end 
+        new_hash
+    end 
+
+        
+end     
